8dbd8b33755a275af0e7b15e7153ce1a15421800
Hintz-Madsen_JPRROI <rasmus.hintzmadsen@gmail.com>
2022-09-28 14:51:29 +0300

    Add painting with mouseover event

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-createGrid(16, 16);
+// createGrid(prompt(), prompt());
+createGrid(100, 100);
 
 function createGrid(x, y) {
-	console.log("test");
 	const grid = document.querySelector(".palikat");
-	console.log(grid);
 
-	const width = "30px";
-	const height = width;
+	const totalWidth = window.innerWidth;
+	console.log(totalWidth);
+
+	const cellWidth = totalWidth / x;
+	const cellHeight = cellWidth;
 
 	for (let iy = 0; iy < y; iy++) {
 		for (let i = 0; i < x; i++) {
 			const div = document.createElement("div");
-			div.style.background = `rgb(${i * 16}, ${iy * 16}, 0)`;
+			// div.style.background = `gray`;
 			div.className = "cell";
-			div.style.width = width;
-			div.style.height = height;
+			div.style.width = `${cellWidth}px`;
+			div.style.height = `${cellHeight}px`;
 			let float = "left";
 			if (i == 0 && iy != 0) {
 				float = "bottom";
 			}
 			if (i == 0 && iy == 0) {
-				div.style.display = "none";
+				continue;
 			}
 			div.style.float = float;
+
+			div.addEventListener("mouseover", (e) => {
+				// console.log(e.target);
+				e.target.className = "highlighted";
+			});
+
 			grid.appendChild(div);
 		}
 	}
